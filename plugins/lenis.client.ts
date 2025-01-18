da97eab96b89164d1634fb543c8387386f8351c3
RajveerSaini0710 <webdevloper0710@gmail.com>
2025-01-19 00:00:00 +0530

    feat: :sparkles: all the files added to the site

---
+++ plugins/lenis.client.ts
@@ -0,0 +1,27 @@
+import Lenis from "lenis";
+import { defineNuxtPlugin } from "#app";
+import { MotionPlugin } from "@vueuse/motion";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+  } as any);
+
+  // Animation loop
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  nuxtApp.vueApp.use(MotionPlugin);
+
+  return {
+    provide: {
+      lenis,
+    },
+  };
+});
