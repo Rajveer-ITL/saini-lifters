1e8cc824500648988f043dfcb6fef58ca0934619
RajveerSaini0710 <webdevloper0710@gmail.com>
2025-03-30 01:00:02 +0530

    fix: :bug: bug fixes for the routing

--- components/base/baseLine.vue
+++ components/base/baseLine.vue
@@ -1,17 +1,17 @@
 <template>
-  <!-- <NuxtLink
+  <NuxtLink
     to="/contact-us"
     class="base-line w-full bg-[#ff4056] flex justify-start md:justify-center items-center overflow-hidden cursor-pointer"
     role="button"
     aria-label="Navigate to contact page"
-  > -->
-  <div
-    class="dark:text-black text-[30px] font-serif text-white py-6 font-extrabold flex items-center whitespace-nowrap scroll-animation"
   >
-    <Construction class="ml-3 w-12 h-12 mr-4 md:mr-6 flex-shrink-0" />
-    Elevate Your Next Project With Our Best Heavy Lifting Services.
-  </div>
-  <!-- </NuxtLink> -->
+    <div
+      class="dark:text-black text-[30px] font-serif text-white py-6 font-extrabold flex items-center whitespace-nowrap scroll-animation"
+    >
+      <Construction class="ml-3 w-12 h-12 mr-4 md:mr-6 flex-shrink-0" />
+      Elevate Your Next Project With Our Best Heavy Lifting Services.
+    </div>
+  </NuxtLink>
 </template>
 
 <script setup>
